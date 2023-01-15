2171ed6db37444631df5bfee2f893f91faec412c
Max Gerasymenko <geramaks03@gmail.com>
2023-01-15 19:29:20 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = onCreateMarkupGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryImgClick)
+
+function onCreateMarkupGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`
+    }).join('')
+}
+
+function onGalleryImgClick(evt) {
+    evt.preventDefault();
+    const currentImg = evt.target.classList.contains('gallery__image');
+    if (!currentImg) {
+        return;
+    }
+    const openModalWindowImg = basicLightbox.create(`<img src="${evt.target.dataset.source}">`,
+        {
+            onShow: openModalWindowImg => {
+                document.addEventListener('keydown', onEscCloseModal);
+            },
+            onClose: openModalWindowImg => {
+                window.removeEventListener('keydown', onEscCloseModal);
+            }
+        })
+    openModalWindowImg.show();
+    function onEscCloseModal(evt) {
+        if (evt.code === 'Escape') {
+            openModalWindowImg.close()
+        }
+    }
+}
