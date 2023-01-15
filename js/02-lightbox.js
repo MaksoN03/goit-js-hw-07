47bb3fdce913182a58396b7fa19013a5a9013558
Max Gerasymenko <geramaks03@gmail.com>
2023-01-15 19:35:14 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery')
+const galleryMarkup = onCreateMarkupGallery(galleryItems)
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+
+galleryContainer.addEventListener('click', onClickImg)
+
+let galleryImg = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+});
+
+function onCreateMarkupGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join('')
+}
+
+function onClickImg(evt) {
+    evt.preventDefault();
+}
